0f16a2c34bc37c31566882a662c34de3f7b6c862
lihanrui2913 <lihanrui20091103@126.com>
2025-02-13 13:02:55 +0800

    打印“正在启动”字样

--- src/lib.rs
+++ src/lib.rs
@@ -4,6 +4,10 @@
 
 #[unsafe(no_mangle)]
 extern "C" fn kmain() -> ! {
+    klog::init();
+
+    log::info!("Reduct OS kernel starting...");
+
     loop {}
 }
 
@@ -12,3 +16,6 @@ fn panic(info: &core::panic::PanicInfo) -> ! {
     log::error!("{}", info);
     loop {}
 }
+
+pub mod klog;
+pub mod serial;
